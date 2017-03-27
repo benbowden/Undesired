9d33dfb5b875b8b8de690ed6e743d17b06c38c6a
uncc bbowden9 <uncc bbowden9>
2017-03-27 13:30:49 -0400

    Can see total cost

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -14,6 +14,7 @@ class StaticController < ApplicationController
 
 
 
+
   end
 
   private 
--- app/models/crop.rb
+++ app/models/crop.rb
@@ -1,3 +1,7 @@
 class Crop < ApplicationRecord
 	belongs_to :user 
+
+	 def total
+    self.amount * self.cost
+  end
 end
