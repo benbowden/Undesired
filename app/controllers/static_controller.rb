f0dcabe9c6ae8e4a0ec1877b1c3f3d6c4c3f55e9
uncc bbowden9 <benbowdene@gmail.com>
2017-05-01 23:31:58 -0400

    fixing farmer error

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -13,10 +13,10 @@ class StaticController < ApplicationController
     @user = current_user
 
     #donator
-    @cropsAll = Crop.where("b_fully_purchased = ?", false)
+    @cropsAll = Crop.where("b_fully_purchased != ?", true)
 
     #farmer
-    @cropsFarmer = current_user.crops.where("b_fully_purchased = ?", false)
+    @cropsFarmer = current_user.crops.where("b_fully_purchased != ?", true)
 
 
     #calc 
