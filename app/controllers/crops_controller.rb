05d013dfe8c614799ac1774ec7940252618c9cb1
uncc bbowden9 <benbowdene@gmail.com>
2017-05-01 23:22:40 -0400

    Farmers can see live info on what they've shipped and what actions they need to take

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -69,6 +69,6 @@ class CropsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def crop_params
-      params.require(:crop).permit(:name, :amount, :cost, :user_id)
+      params.require(:crop).permit(:name, :amount, :cost, :user_id, :b_delivered)
     end
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,12 +6,43 @@ class StaticController < ApplicationController
   end
 
   def dashboard
+
+    #setup
   	@b_isFarmer = current_user.isFarmer
   	@userTest = current_user
     @user = current_user
 
-    @cropsFarmer = current_user.crops.all 
-    @cropsAll = Crop.where("b_fully_purchased = ?", false) 
+    #donator
+    @cropsAll = Crop.where("b_fully_purchased = ?", false)
+
+    #farmer
+    @cropsFarmer = current_user.crops.where("b_fully_purchased = ?", false)
+
+
+    #calc 
+    @cropsThatNeedToBeDelivered = current_user.crops.where("b_fully_purchased = ?", true).count
+    @cropsDelivered = current_user.crops.where("b_delivered = ?", true)
+    @cropsDeliveredCount = @cropsDelivered.count
+    
+    #calc total
+    @totalBalance = 0
+    @cropsDelivered.each do |f|
+      @totalBalance += f.total
+
+    end
+
+    
+
+    if(@cropsThatNeedToBeDelivered.nil?)
+      @cropsThatNeedToBeDelivered = 0
+    end
+
+    if (@cropsDelivered.nil?)
+      @cropsShippedCount = 0
+    end
+
+
+
   end
 
 
