7a3c31c70295034585cd59e7de90aa4504304861
uncc bbowden9 <benbowdene@gmail.com>
2017-05-08 00:36:36 -0400

    Added homepage function to allow to pass email to signup flow

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,8 @@
-class RegistrationsController < ApplicationController
-  def after_sign_up_path_for
-  	"http://www.google.com"
-  	
-  end
-
-  def after_sign_in_path_for
-  	"http://www.tubefilter.com"
-  end
+class RegistrationsController <  Devise::RegistrationsController
+	def new
+		@emailFromParams = params[:email] 
+		super
+	end
+			
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
 
   resources :crops
   resources :produces
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
   resources :charges #stripe
 
   get 'registrations/after_sign_up_path_for'
   get 'static/dashboard'
   get 'static/orders'
+
+
+
 
   root 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
